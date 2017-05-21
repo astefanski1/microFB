e87b9476d6f1dac55032e7b8fb1dce6c03cce514
astefanski1 <aleksander.stefanski@outlook.com>
2017-05-21 23:39:11 +0200

    added all views

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/*jshint node: true */
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -12,7 +13,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'handlebars');
+app.set('view engine', 'hbs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
+/*jshint node: true */
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/authentication', function(req, res, next) {
+  res.render('authentication', { title: 'Express' });
+});
+
+router.get('/wall', function(req, res, next) {
+  res.render('wall', { title: 'Express' });
 });
 
 module.exports = router;
